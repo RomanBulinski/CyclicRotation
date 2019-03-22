df65820c9219aad9111609fd446e46dad1e4204f
Roman Bulinski <aboandrus@gmail.com>
2019-03-22 06:53:17 +0100

    Code refactor.

--- src/main/java/CycRotation.java
+++ src/main/java/CycRotation.java
@@ -1,11 +1,11 @@
 public class CycRotation {
 
-    public int[] solution(int[] A, int K){
+    public int[] solution(int[] A, int K) {
         int counter = 0;
-        while(counter < K  ){
-            int temp = A[A.length-1];
-            for ( int i =A.length-1; i>0; i--   ){
-                A[i] = A[i-1];
+        while (counter < K) {
+            int temp = A[A.length - 1];
+            for (int i = A.length - 1; i > 0; i--) {
+                A[i] = A[i - 1];
             }
             A[0] = temp;
             counter++;
--- src/test/java/TestCyclo.java
+++ src/test/java/TestCyclo.java
@@ -5,22 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class TestCyclo {
 
     @Test
-    public void test(){
+    public void test() {
         CycRotation cycRotation = new CycRotation();
         int[] A = {3, 8, 9, 7, 6};
         int K = 3;
         int[] B = {9, 7, 6, 3, 8};
-        assertArrayEquals( B, cycRotation.solution(A,K) );
+        assertArrayEquals(B, cycRotation.solution(A, K));
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         CycRotation cycRotation = new CycRotation();
-        int[] A = {1,2,3,4,5};
+        int[] A = {1, 2, 3, 4, 5};
         int K = 1;
-        int[] B = {5,1,2,3,4};
-        assertArrayEquals( B, cycRotation.solution(A,K) );
+        int[] B = {5, 1, 2, 3, 4};
+        assertArrayEquals(B, cycRotation.solution(A, K));
     }
-
 
 }
